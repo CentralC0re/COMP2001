1242e3eac92a31f8bbf3e19a555a2c94d5bb0cf1
CentralC0re <theepicgameminer@gmail.com>
2024-10-31 15:08:35 +0000

    Created View
    Titled "Basic View", contains most important details about Trails

---
+++ SQL/CreateBasicView.sql
@@ -0,0 +1,11 @@
+CREATE VIEW CW1.BasicTrail AS
+SELECT NameVal, Difficulty, LengthVal, RouteType, EstimatedTime, CW1.TagTable.TagName, CW1.LocationTable.Country, 
+CW1.LocationTable.Region, CW1.LocationTable.Area
+
+FROM CW1.TrailTable
+INNER JOIN CW1.TagTrailTable
+ON CW1.TrailTable.TrailID = CW1.TagTrailTable.TrailID
+INNER JOIN CW1.TagTable
+ON CW1.TagTrailTable.TagID = CW1.TagTable.TagID
+INNER JOIN CW1.LocationTable
+ON CW1.TrailTable.LocationID = CW1.LocationTable.LocationID;
